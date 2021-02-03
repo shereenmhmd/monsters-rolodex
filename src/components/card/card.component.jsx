cace011649fcf0d0aaf740a266ff7ef50638875c
Shereen <Shereen.mhmd@outlook.com>
2021-02-03 19:45:02 +0200

    first commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './card.styles.css';
+export const Card = (props) => <div className = "card-container">
+    <img src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`} alt=""/>
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+</div>
